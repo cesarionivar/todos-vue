53018e9712f1e6dc02bdbad4c774375daa0b4a2a
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-02-17 00:51:36 -0400

    :sparkles: Handle todo actions

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <script setup>
 import { ref } from 'vue';
+import TodoList from './classes/TodoList';
 
-const todos = ref([]);
+const todoList = new TodoList();
+const todos = ref(todoList.getInitialTodos());
 const todo = ref('');
 
 const addTodo = () => {
-  todos.value.push(todo.value);
+  if (todo.value === '') return null;
+  todoList.addTodo(todo.value);
   todo.value = '';
 };
 
-const removeTodo = (todo) => {
-  console.log('Remove', todo);
+const removeTodo = (id) => {
+  todos.value = todoList.removeTodo(id);
+};
+
+const markAsCompleted = (id) => {
+  todos.value = todoList.markAsCompleted(id);
+};
+
+const pendingTodos = () => {
+  return todos.value.filter((todo) => !todo.completed).length;
 };
 </script>
 
 <template>
   <div class="w-4/5 mx-auto">
-    <h1 class="text-3xl font-bold my-10">Todos ({{ todos.length }})</h1>
+    <h1 class="text-3xl font-bold my-10">Todos</h1>
     <section>
       <form>
         <input
@@ -35,15 +46,23 @@ const removeTodo = (todo) => {
         </button>
       </form>
       <div class="flex flex-col">
-        <h2 class="text-xl font-bold my-4">Pending Todos</h2>
+        <h2 class="text-xl font-bold my-4">
+          Pending Todos ({{ pendingTodos() }})
+        </h2>
         <ul class="ml-8">
           <li
-            class="list-decimal"
-            :key="todo"
             v-for="todo in todos"
-            @click="removeTodo"
+            :class="[
+              todo.completed ? 'line-through' : '',
+              'list-decimal',
+              'cursor-pointer',
+              'select-none',
+            ]"
+            :key="todo"
+            @click="markAsCompleted(todo.id)"
+            @dblclick="removeTodo(todo.id)"
           >
-            {{ todo }}
+            {{ todo.desc }}
           </li>
         </ul>
       </div>
